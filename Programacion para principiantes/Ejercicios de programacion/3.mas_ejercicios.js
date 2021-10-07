74ac2bf0acd0e4ab40ca77819122a3e0ea3e0f07
yefry <yefryrodelo@gmail.com>
2021-10-06 22:05:36 -0500

    inicializacion de POO en javascript basico

--- Programacion para principiantes/Ejercicios de programacion/3.mas_ejercicios.js
+++ Programacion para principiantes/Ejercicios de programacion/3.mas_ejercicios.js
@@ -2,8 +2,9 @@
 // 1+2+3+4+5+6+7+8+9+10
 //resultado de eso
 
-let nombre = "Yefry";
+let numeros = [1,2];
 
-let letras = nombre.length;
-console.log(letras);
-
+for (let i = 0; i <= numeros.length; i++) {
+    
+    console.log(numeros[i])
+}
