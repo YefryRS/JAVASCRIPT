212da75304682efe72aef356912fb2b5d7b7f8ba
yefry <yefryrodelo@gmail.com>
2021-10-14 12:42:34 -0500

    mas ejercicios de programacion

--- Programacion para principiantes/Ejercicios de programacion/3.mas_ejercicios.js
+++ Programacion para principiantes/Ejercicios de programacion/3.mas_ejercicios.js
@@ -1,10 +1,51 @@
-// 10 
-// 1+2+3+4+5+6+7+8+9+10
-//resultado de eso
+/* Ejercicios de Cadenas
 
-let numeros = [1,2];
+Ejercicio 1
+Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido. */
 
-for (let i = 0; i <= numeros.length; i++) {
-    
-    console.log(numeros[i])
+/* let usuario = "Yefry";
+let repeatUsuario = 3;
+ */
+
+//let usuarioCompleto =[];
+
+
+/* while(repeatUsuario > 0) {
+    usuarioCompleto.push(usuario)
+    repeatUsuario --;
 }
+console.log(usuarioCompleto); */
+
+
+class nombreUsuario {
+    constructor({
+        usuario,
+        repeatUsuario,
+        usuarioCompleto = [],
+    }){
+        this.usuario = usuario;
+        this.repeatUsuario = repeatUsuario;
+        this._usuarioCompleto = usuarioCompleto
+    }
+    get usuarioCompleto() {
+        return this._usuarioCompleto;
+    }
+    set usuarioCompleto(newUser) {
+        this._usuarioCompleto = newUser;
+    }
+
+    RepeatUser() {
+        while(this.repeatUsuario > 0) {
+            this.usuarioCompleto.push(this.usuario)
+            this.repeatUsuario --;
+        }
+        console.log(this.usuarioCompleto);
+    }
+}
+
+let Yefry = new nombreUsuario({
+    usuario: "Yefry",
+    repeatUsuario: 3,
+})
+Yefry.RepeatUser();
+
