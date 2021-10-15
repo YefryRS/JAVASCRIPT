4f4f56dec5aeed375ca88781310ca3bd1869fff6
yefry <yefryrodelo@gmail.com>
2021-10-15 06:37:02 -0500

    version quien sabe

--- Programacion para principiantes/Ejercicios de programacion/3.mas_ejercicios.js
+++ Programacion para principiantes/Ejercicios de programacion/3.mas_ejercicios.js
@@ -3,19 +3,6 @@
 Ejercicio 1
 Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido. */
 
-/* let usuario = "Yefry";
-let repeatUsuario = 3;
- */
-
-//let usuarioCompleto =[];
-
-
-/* while(repeatUsuario > 0) {
-    usuarioCompleto.push(usuario)
-    repeatUsuario --;
-}
-console.log(usuarioCompleto); */
-
 
 class nombreUsuario {
     constructor({
@@ -48,4 +35,32 @@ let Yefry = new nombreUsuario({
     repeatUsuario: 3,
 })
 Yefry.RepeatUser();
+
+
+
+/* Ejercicio 2
+Escribir un programa que pregunte el nombre completo del usuario en la consola y después muestre por pantalla el nombre completo del usuario tres veces, una con todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo con la primera letra del nombre y de los apellidos en mayúscula. El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
+ */
+
+
+function user(nombreUsuario) {
+    let nombre = nombreUsuario;
+    nombre = nombre.toUpperCase();
+    let nameUser = [];
+    nameUser.push(nombre);
+    nombre = nombre.toLocaleLowerCase();
+    nameUser.push(nombre);
+    console.log(nameUser);
+}
+
+
+let cadena = "Pontraseña";
+let cadena2 = "Contraseña";
+
+if (cadena2 == cadena) {
+    console.log("introdujiste la misma contraseña")
+}
+else {
+    console.log("Te equivocastes perro")
+}
 
